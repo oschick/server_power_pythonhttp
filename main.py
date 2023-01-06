634a55527274c6d15cc002adf70bc728de4ba8a6
oschick <git@schicketanz.com.de>
2023-01-06 17:56:27 +0100

    Change to header

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ ha_webhook = os.environ['HA_WEBHOOK']
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        data = requests.post(ha_webhook)
+        data = requests.post(ha_webhook, timeout=5)
         if data.status_code == 200:
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(bytes("<html><head><title>Plex Server</title></head>", "utf-8"))
             self.wfile.write(bytes("<body>", "utf-8"))
-            self.wfile.write(bytes("<p>Befehl zum Starten wurde gesendet.</p>", "utf-8"))
-            self.wfile.write(bytes("<p>Plex sollte in ca. 4Min verfuegbar sein.</p>", "utf-8"))
+            self.wfile.write(bytes("<h1>Befehl zum Starten wurde gesendet.</h1>", "utf-8"))
+            self.wfile.write(bytes("<h1>Plex sollte in ca. 4Min verfuegbar sein.</h1>", "utf-8"))
             self.wfile.write(bytes("</body></html>", "utf-8"))
         else:
             self.send_response(500)
